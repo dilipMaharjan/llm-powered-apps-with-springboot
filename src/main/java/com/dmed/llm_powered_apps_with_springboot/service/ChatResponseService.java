c41eb566ee2277ddc3956b4b279da2a759b8a352
dilipMaharjan <dilipmaharjann@gmail.com>
2026-01-20 19:11:52 -0600

    ChatOptions

--- src/main/java/com/dmed/llm_powered_apps_with_springboot/service/ChatResponseService.java
+++ src/main/java/com/dmed/llm_powered_apps_with_springboot/service/ChatResponseService.java
@@ -3,6 +3,7 @@ package com.dmed.llm_powered_apps_with_springboot.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.ChatClientResponse;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -25,8 +26,16 @@ public class ChatResponseService {
     }
 
     public ChatClientResponse getGeneralChatClientResponse(String prompt) {
+        ChatOptions chatOptions = ChatOptions.builder()
+//                .model("llama3.2")
+                // Deterministic responses
+//                .temperature(0.0)
+                // More creative responses
+                .temperature(0.7)
+                .build();
         return Objects.requireNonNull(generalChatClient.
                 prompt(prompt)
+                .options(chatOptions)
                 .call()
                 .chatClientResponse());
     }
