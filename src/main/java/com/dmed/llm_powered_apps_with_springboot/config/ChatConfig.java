603f8d471c69bed3581be4650005fa5bc8f3c277
dilipMaharjan <dilipmaharjann@gmail.com>
2026-01-27 19:57:05 -0600

    Advisors

--- src/main/java/com/dmed/llm_powered_apps_with_springboot/config/ChatConfig.java
+++ src/main/java/com/dmed/llm_powered_apps_with_springboot/config/ChatConfig.java
@@ -2,6 +2,7 @@ package com.dmed.llm_powered_apps_with_springboot.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +18,14 @@ public class ChatConfig {
     @Bean
     public ChatClient promptChatClient(ChatClient.Builder chatClientBuilder) {
         log.info("Configuring ChatClient bean for prompt assistant");
+        chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor());
         return chatClientBuilder.build();
     }
     @Bean
     public ChatClient generalChatClient(ChatClient.Builder chatClientBuilder) {
         log.info("Configuring ChatClient bean for general assistant");
         chatClientBuilder.defaultSystem("You're a helpful assistant.");
+        chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor());
         return chatClientBuilder.build();
     }
 
@@ -30,6 +33,7 @@ public class ChatConfig {
     public ChatClient airlineChatClient(ChatClient.Builder chatClientBuilder) {
         log.info("Configuring ChatClient bean for airline assistant");
         chatClientBuilder.defaultSystem(airlineAssistantSystemMessage);
+        chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor());
         return chatClientBuilder.build();
     }
 }
