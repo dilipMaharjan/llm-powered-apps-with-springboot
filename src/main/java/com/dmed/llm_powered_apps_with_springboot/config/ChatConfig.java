a4f02895e9e70a13c2271cb6d20c589329de2807
dilipMaharjan <dilipmaharjann@gmail.com>
2026-01-29 15:59:12 -0600

    Clean up and structured output

--- src/main/java/com/dmed/llm_powered_apps_with_springboot/config/ChatConfig.java
+++ src/main/java/com/dmed/llm_powered_apps_with_springboot/config/ChatConfig.java
@@ -2,18 +2,12 @@ package com.dmed.llm_powered_apps_with_springboot.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 @Configuration
 @Slf4j
 public class ChatConfig {
-
-    @Value("classpath:airline_assistant_system_message.txt")
-    Resource airlineAssistantSystemMessage;
-
     @Bean
     public ChatClient generalChatClient(ChatClient.Builder chatClientBuilder) {
         log.info("Configuring ChatClient bean for general assistant");
