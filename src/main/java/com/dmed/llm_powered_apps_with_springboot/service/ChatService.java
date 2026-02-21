1a48e205cd7a08ad86a6d37a35e3fc8fcf506afd
dilipMaharjan <dilipmaharjann@gmail.com>
2026-02-20 20:52:09 -0600

    Rag Initial setup

--- src/main/java/com/dmed/llm_powered_apps_with_springboot/service/ChatService.java
+++ src/main/java/com/dmed/llm_powered_apps_with_springboot/service/ChatService.java
@@ -1,7 +1,6 @@
 package com.dmed.llm_powered_apps_with_springboot.service;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -11,26 +10,13 @@ public class ChatService {
 
     private final ChatClient generalChatClient;
 
-    private final ChatClient airlineChatClient;
 
-    public ChatService(@Qualifier("generalChatClient") ChatClient generalChatClient,
-                       @Qualifier("airlineChatClient") ChatClient airlineChatClient) {
+    public ChatService(ChatClient generalChatClient) {
         this.generalChatClient = generalChatClient;
-        this.airlineChatClient = airlineChatClient;
     }
 
     public String getGeneralChatResponse(String prompt) {
         return Objects.requireNonNull(generalChatClient.
-                        prompt(prompt)
-                        .call()
-                        .chatResponse())
-                .getResult()
-                .getOutput()
-                .getText();
-    }
-
-    public String getAirlineChatResponse(String prompt) {
-        return Objects.requireNonNull(airlineChatClient.
                         prompt(prompt)
                         .call()
                         .chatResponse())
